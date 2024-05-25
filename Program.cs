29d8b093d44fa7213b88214269047997e02d9184
neufrankie <neu.vietanh272@gmail.com>
2024-05-25 21:01:00 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿﻿using System;
+
+namespace MenuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = -1;
+            while (choice != 0)
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Ve hinh tam giac");
+                Console.WriteLine("2. Ve hinh vuong");
+                Console.WriteLine("3. Ve hinh chu nhat");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Nhap vao lua chon cua ban: ");
+                choice = Int32.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Ve hinh tam giac");
+                        Console.WriteLine("******");
+                        Console.WriteLine("*****");
+                        Console.WriteLine("****");
+                        Console.WriteLine("***");
+                        Console.WriteLine("**");
+                        Console.WriteLine("*");
+                        break;
+                    case 2:
+                        Console.WriteLine("Ve hinh vuong");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        break;
+                    case 3:
+                        Console.WriteLine("Ve hinh chu nhat");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Thoat khoi chuong trinh!");
+                        break;
+                }
+
+            }
+        }
+    }
+}
